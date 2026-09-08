9f51e3a3e7e3aab6fd39cfe0fc0849fa5cd7b3bb
Eddie Teixeira <esdrasevt@gmail.com>
2026-09-08 15:51:23 -0500

    feat: add file:line:column clipboard yank function and keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -5,12 +5,43 @@ vim.api.nvim_set_keymap(
   { noremap = true }
  )
 
+-- Use <leader>y as a clipboard yank operator, so <leader>yiw copies the current word.
 vim.api.nvim_set_keymap(
   "n",
   "<leader>y",
   "\"*y",
   { noremap = true }
  )
+
+-- Copy the current file path, line, and column to the system clipboard.
+local function copy_location()
+  local buffer_name = vim.api.nvim_buf_get_name(0)
+
+  if buffer_name == "" then
+    vim.notify("Current buffer has no file name", vim.log.levels.WARN)
+    return
+  end
+
+  local file = vim.fn.fnamemodify(buffer_name, ":.")
+  local line = vim.fn.line(".")
+  local column = vim.fn.col(".")
+  local location = string.format("%s:%d:%d", file, line, column)
+
+  vim.fn.setreg("+", location)
+  vim.notify("Copied: " .. location)
+end
+
+vim.api.nvim_create_user_command("CopyLocation", copy_location, {
+  desc = "Copy file:line:column to the system clipboard",
+})
+
+-- <leader>yl copies a reference such as path/to/file.lua:12:8.
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>yl",
+  "<cmd>CopyLocation<cr>",
+  { noremap = true }
+)
 
  vim.api.nvim_set_keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { noremap = true })
 
@@ -57,4 +88,3 @@ vim.api.nvim_set_keymap(
   "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
   { noremap = true }
 )
-
